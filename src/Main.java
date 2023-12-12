5e32782b31d9c773e99d5856e619a4bb130cc701
Ekaterina Turkina <katrina.turkina@gmail.com>
2023-12-12 12:33:42 +0300

    changesMain

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int annuityPayment = (int) service.calculate(9.99, 1000000, 3);
+        int annuityPayment = (int) service.calculate(9.99, 1_000_000, 3);
         System.out.println(annuityPayment);
     }
 }
